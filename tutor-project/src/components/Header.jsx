514ea10b0257f7e3ba37955612b9820b37a31f9e
Zach <peganovzacharia@gmail.com>
2025-09-30 13:02:51 -0700

    Added header

---
+++ tutor-project/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+export function Header() {
+    return (
+        <>
+        <div className="header">
+            <div className="left-section">About</div>
+            <div className="middle-section">TUTORMI</div>
+            <div className="right-section">Contact Us</div>
+            </div>
+        </>
+    );
+}
